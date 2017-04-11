6004cf7fb9348ec6cbc7843e10088eb93721e563
mlynnes <micaelalsassano@gmail.com>
2017-04-11 11:47:55 -0400

    Complete ui.js
    Added: signInSuccess,
      signInFailure,
      changePasswordSuccess,
      changePasswordFailure,
      signOutSuccess,
      signOutFailure
    
      functions and ways to test them.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -9,3 +9,39 @@ const signUpSuccess = (data) => {
 const signUpFailure = (error) => {
   console.error(error)
 }
+
+const signInSuccess = (response) => {
+  console.log('You signed in. and response is', response)
+  store.user = response.user
+}
+
+const signInFailure = (data) => {
+  console.log('You failed to sign in')
+}
+
+const changePasswordSuccess = (response) => {
+  console.log('response is', response)
+}
+
+const changePasswordFailure = (error) => {
+  console.error(error)
+}
+
+const signOutSuccess = (success) => {
+  console.log('Succesful sign out')
+}
+
+const signOutFailure = (error) => {
+  console.error(error)
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  changePasswordSuccess,
+  changePasswordFailure,
+  signOutSuccess,
+  signOutFailure
+}
