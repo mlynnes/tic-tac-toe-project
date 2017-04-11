ebdcafe562b66c6f07a6ad1dcf209d780c21c09e
mlynnes <micaelalsassano@gmail.com>
2017-04-11 11:22:55 -0400

    Events and Handlers
    Added events for onSignIn, onChangePassord, and onSignOut.
    Each with a console.log to test it.
    Click handlers were also added to the file.

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -12,3 +12,40 @@ const onSignUp = function (event) {
     .then(ui.signUpSuccess)
     .catch(ui.signUpFailure)
 }
+
+const onSignIn = function (event) {
+  const data = getFormFields(this)
+  event.preventDefault()
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+
+const onChangePassword = function (event) {
+  const data = getFormFields(this)
+  event.preventDefault()
+  console.log('inside onChangePassword')
+  api.changePassword(data)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
+}
+
+const onSignOut = function (event) {
+  const data = getFormFields(this)
+  event.preventDefault()
+  console.log('inside onSignOut')
+  api.signOut(data)
+  .then(ui.signOutSuccess)
+  .catch(ui.signOutFailure)
+}
+
+const addHandlers = () => {
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
+  $('#change-password').on('submit', onChangePassword)
+  $('#sign-out').on('submit', onSignOut)
+}
+
+module.exports = {
+  addHandlers
+}
